785363d9b48036ed0f42804d04cc1c8b7435f90d
hela hached <hela.hached@esen.tn>
2024-02-01 20:44:58 +0100

    complete classe, modules et datetime

---
+++ Seance4/Calcul.py
@@ -0,0 +1,9 @@
+def somme(x=0,y=0):
+    return x+y
+
+
+def produit(x=0,y=0):
+    return x*y
+
+def calcul(x=0,y=0):
+    return x*y,x+y
---
+++ Seance4/test_utilisation_calcul.py
@@ -0,0 +1,9 @@
+import Calcul as cal
+#pour importer une fonction specifique
+#form Calcul import somme 
+
+print('somme=%d',cal.somme(5,10))
+
+print(cal.produit(5,10))
+p,s=cal.calcul(10,20)
+print("{0:d}+{1:d}={2:d} , {0:d}*{1:d}={3:d}".format(10,20,s,p))
